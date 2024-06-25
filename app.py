d045ef96277b0aa9cb7e807059d3270aafa3a007
user <k@163.com>
2024-06-25 08:39:10 +0800

    更新代码

--- app.py
+++ app.py
@@ -1,85 +1,11 @@
-"""
-This script creates an interactive web demo for the ChatGLM3-6B model using Gradio,
-a Python library for building quick and easy UI components for machine learning models.
-It's designed to showcase the capabilities of the ChatGLM3-6B model in a user-friendly interface,
-allowing users to interact with the model through a chat-like interface.
 
-Usage:
-- Run the script to start the Gradio web server.
-- Interact with the model by typing questions and receiving responses.
-
-Requirements:
-- Gradio (required for 4.13.0 and later, 3.x is not support now) should be installed.
-
-Note: The script includes a modification to the Chatbot's postprocess method to handle markdown to HTML conversion,
-ensuring that the chat interface displays formatted text correctly.
-
-"""
 
 import os
 import gradio as gr
-import torch
+
 from threading import Thread
 import random
 import time
-
-# from typing import Union, Annotated
-# from pathlib import Path
-# from peft import AutoPeftModelForCausalLM, PeftModelForCausalLM
-# from transformers import (
-#     AutoModelForCausalLM,
-#     AutoTokenizer,
-#     PreTrainedModel,
-#     PreTrainedTokenizer,
-#     PreTrainedTokenizerFast,
-#     StoppingCriteria,
-#     StoppingCriteriaList,
-#     TextIteratorStreamer
-# )
-
-# ModelType = Union[PreTrainedModel, PeftModelForCausalLM]
-# TokenizerType = Union[PreTrainedTokenizer, PreTrainedTokenizerFast]
-
-# MODEL_PATH = os.environ.get('MODEL_PATH', 'THUDM/chatglm3-6b')
-# TOKENIZER_PATH = os.environ.get("TOKENIZER_PATH", MODEL_PATH)
-
-
-# def _resolve_path(path: Union[str, Path]) -> Path:
-#     return Path(path).expanduser().resolve()
-
-
-# def load_model_and_tokenizer(
-#         model_dir: Union[str, Path], trust_remote_code: bool = True
-# ) -> tuple[ModelType, TokenizerType]:
-#     model_dir = _resolve_path(model_dir)
-#     if (model_dir / 'adapter_config.json').exists():
-#         model = AutoPeftModelForCausalLM.from_pretrained(
-#             model_dir, trust_remote_code=trust_remote_code, device_map='auto'
-#         )
-#         tokenizer_dir = model.peft_config['default'].base_model_name_or_path
-#     else:
-#         model = AutoModelForCausalLM.from_pretrained(
-#             model_dir, trust_remote_code=trust_remote_code, device_map='auto'
-#         )
-#         tokenizer_dir = model_dir
-#     tokenizer = AutoTokenizer.from_pretrained(
-#         tokenizer_dir, trust_remote_code=trust_remote_code
-#     )
-#     return model, tokenizer
-
-
-# model, tokenizer = load_model_and_tokenizer(MODEL_PATH, trust_remote_code=True)
-
-
-# class StopOnTokens(StoppingCriteria):
-#     def __call__(self, input_ids: torch.LongTensor, scores: torch.FloatTensor, **kwargs) -> bool:
-#         stop_ids = [0, 2]
-#         for stop_id in stop_ids:
-#             if input_ids[0][-1] == stop_id:
-#                 return True
-#         return False
-
-
 def parse_text(text):
     lines = text.split("\n")
     lines = [line for line in lines if line != ""]
@@ -113,22 +39,7 @@ def parse_text(text):
 
 
 def predict(history, max_length, top_p, temperature):
-    # stop = StopOnTokens()
-    # messages = []
-    # for idx, (user_msg, model_msg) in enumerate(history):
-    #     if idx == len(history) - 1 and not model_msg:
-    #         messages.append({"role": "user", "content": user_msg})
-    #         break
-    #     if user_msg:
-    #         messages.append({"role": "user", "content": user_msg})
-    #     if model_msg:
-    #         messages.append({"role": "assistant", "content": model_msg})
-
-    # print("\n\n====conversation====\n", messages)
-    # model_inputs = tokenizer.apply_chat_template(messages,
-    #                                              add_generation_prompt=True,
-    #                                              tokenize=True,
-    #                                              return_tensors="pt").to(next(model.parameters()).device)
+    
     # streamer = TextIteratorStreamer(tokenizer, timeout=60, skip_prompt=True, skip_special_tokens=True)
     # generate_kwargs = {
     #     "input_ids": model_inputs,
@@ -156,8 +67,24 @@ def predict(history, max_length, top_p, temperature):
     #         yield history
 
 
+def chat_response(message, history):
+    # 这里可以添加你的聊天机器人逻辑
+    responses = ["回复1", "回复2", "回复3"]  # 示例回复列表
+    
+    for response in responses:
+        yield "", response
+
+def bot(history,max_length, top_p, temperature):
+        user_message = history[-1][0]
+        for i, response in enumerate(chat_response(user_message, history)):
+            if i == 0:
+                history[-1][1] = response[1]
+            else:
+                history.append([None, response[1]])
+            yield history
+
 with gr.Blocks() as demo:
-    gr.HTML("""<h1 align="center">ChatGLM3-6B Gradio Simple Demo</h1>""")
+    gr.HTML("""<h1 align="center">Web Automation Demo</h1>""")
     chatbot = gr.Chatbot()
 
     with gr.Row():
@@ -178,9 +105,9 @@ with gr.Blocks() as demo:
 
 
     submitBtn.click(user, [user_input, chatbot], [user_input, chatbot], queue=False).then(
-        predict, [chatbot, max_length, top_p, temperature], chatbot
+        bot, [chatbot, max_length, top_p, temperature], chatbot
     )
     emptyBtn.click(lambda: None, None, chatbot, queue=False)
 
 demo.queue()
-demo.launch(server_name="0.0.0.0", server_port=7870, inbrowser=True, share=False)
+demo.launch(server_name="0.0.0.0", server_port=5001, inbrowser=True, share=False)
